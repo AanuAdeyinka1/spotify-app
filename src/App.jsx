141dd2865454ad5fe2280105a65555ba6e2b72e1
Aanu lawal <lawalaanu14@gmail.com>
2024-03-28 07:41:16 +0100

    created path

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import logo from "./logo.svg";
-import { RouterProvider, createBrowserRouter} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import "./App.css";
 import { LandingPageHeader } from "./component/LandingPage/LandingPage.js"
 import { LoginPage } from "./component/LoginPage/Login.jsx";
 
 
-const router = createBrowserRouter([
-  {
-    path: '',
-    component: LandingPageHeader,
-  },
-  {
-    path: "/login",
-    Element: <LoginPage />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '',
+//     component: LandingPageHeader,
+//   },
+//   {
+//     path: "/login",
+//     Element: <LoginPage />,
+//   },
+// ]);
+
+//  export function App() {
+//   // return <RouterProvider router ={router} />
+//   return <LandingPageHeader />;
+// }
+
+// export default App;
 
 export function App() {
-  // return <RouterProvider router ={router} />
-  return <LandingPageHeader />;
+ 
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPageHeader />} />
+
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
